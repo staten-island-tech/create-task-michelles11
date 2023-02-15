7925a05aa73a55d3faf0a5487cf04f8bb43aef6c
michelles11 <113190099+michelles11@users.noreply.github.com>
2023-02-15 10:16:23 -0500

    2.15.23

---
+++ apcs-create-task/js/inventory.js
@@ -0,0 +1,102 @@
+const inventory = [
+    {
+        book: "Fantastic Mr.Fox",
+        author:"Roald Dahl",
+        image:,
+    },
+    {
+        book:"Pride and Prejudice",
+        author:"Jane Austen",
+        image:,
+    },
+    {
+        book:"Chrysanthemum",
+        author:"Kevin Henkes",
+        image:,
+    },
+    {
+        book:"Goosebumps",
+        author:"R.L.Stein",
+        image:,
+    },
+    {
+        book:"Harry Potter:The Prisoner of Azkaban",
+        author:"J.K.Rowling",
+        image:,
+    },
+    {
+        book:"Echo",
+        author:"Pam Munuez",
+        image:,
+    },
+    {
+        book:"Moby Dick",
+        author:"Charles Dickens",
+        image:,
+    },
+    {
+        book:"Lord of The Rings",
+        author:"J.R.R.Tolkien",
+        image:,
+    },
+    {
+        book:"Flowers For Algernon",
+        author:"Daniel Keyes",
+        image:,
+    },
+    {
+        book:"Klara and the Sun",
+        author:"Kazuo Ishiguro",
+        image:,
+    },
+    {
+        book:"The Apocolypse Seven",
+        author:"Gene Doucette",
+        image:,
+    },
+    {
+        book:"",
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+    {
+        book:,
+        author:,
+        image:,
+    },
+]
